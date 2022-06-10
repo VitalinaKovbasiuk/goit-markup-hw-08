7b4c75d407afd5b561264661021ab4ec69d7c4a2
VitalinaKovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-06-10 16:02:20 +0300

    Додаю tablet хедер

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
-(() => {data-menu-open
+(() => {
   const refs = {
-    openMenuBtn: document.querySelector('[]'),
+    openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
-     };
+  };
 
   refs.openMenuBtn.addEventListener('click', toggleModal);
   refs.closeMenuBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.menu.classList.toggle('is-open');
-    }
+  }
 })();
